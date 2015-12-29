be00483de980edbbf6ef00d0b0a6f14ca1b3dc51
Josiah Gaskin <josiahgaskin@gmail.com>
2015-12-28 21:49:05 -0800

    reading input for day 19

---
+++ src/day19/mod.rs
@@ -0,0 +1,45 @@
+use std::str;
+use chomp::*;
+use chomp::ascii::{skip_whitespace,is_uppercase,is_lowercase};
+
+fn maybe_lower(i: Input<u8>) -> U8Result<String> {
+	option(i, |i| parse!{i;
+		let s = satisfy(is_lowercase);
+		ret format!("{}", s as char)
+	}, "".to_string())
+}
+
+fn atom(i: Input<u8>) -> U8Result<String> {
+	parse!{i;
+		let s1 = satisfy(is_uppercase);
+		let s2 = maybe_lower();
+		ret format!("{}{}", s1 as char, s2)
+	}
+}
+
+fn rule(i: Input<u8>) -> U8Result<(String, Vec<String>)> {
+	parse!{i;
+		let from = atom();
+				   string(b" => ");
+		let to   = many1(atom);
+				   skip_whitespace();
+		ret (from, to)
+	}
+}
+
+fn all_rules(i: Input<u8>) -> U8Result<Vec<(String, Vec<String>)>> {
+	parse!{i;
+		let v = many1(rule);
+		ret v
+	}
+}
+
+
+pub fn part1(input: String) -> String {
+	format!("{:?}", parse_only(all_rules, input.as_bytes()).unwrap())
+}
+
+
+pub fn part2(input: String) -> String {
+	"part2".to_string()
+}
