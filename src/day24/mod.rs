c1332f223005731967b5b9aec5e707c7c0e3abe9
Josiah Gaskin <josiahgaskin@gmail.com>
2016-01-04 15:43:54 -0800

    Giving up on generating full powerset

--- src/day24/mod.rs
+++ src/day24/mod.rs
@@ -1,6 +1,6 @@
 use chomp::*;
 use chomp::ascii::{skip_whitespace,decimal};
-use std::collections::HashSet;
+use std::collections::LinkedList;
 
 fn line(i: Input<u8>) -> U8Result<usize> {
 	parse!{i;
@@ -17,216 +17,257 @@ fn all_lines(i: Input<u8>) -> U8Result<Vec<usize>> {
 	}
 }
 
-struct Bloom64 {
-	bits: Vec<u64>,
-	n: u64,
-	count: usize,
-}
+// struct Bloom64 {
+// 	bits: Vec<u64>,
+// 	bits2: Vec<u64>,
+// 	n: u64,
+// 	count: usize,
+// }
+//
+// impl Bloom64 {
+// 	fn with_cardinality(n: usize) -> Bloom64 {
+// 		Bloom64 {
+// 			bits: vec![0; n],
+// 			bits2: vec![0; n],
+// 			n: 64 * n as u64,
+// 			count: 0,
+// 		}
+// 	}
+//
+// 	fn contains(&self, hash_a: u64, hash_b: u64, hash_c: u64) -> bool {
+// 		let ia = ((hash_a % self.n) / 64) as usize;
+// 		let ra = (hash_a % self.n) % 64;
+// 		let a = self.bits[ia] & 1 << ra != 0;
+//
+// 		let ib = ((hash_b % self.n) / 64) as usize;
+// 		let rb = (hash_b % self.n) % 64;
+// 		let b = self.bits[ib] & 1 << rb != 0;
+//
+// 		let ic = ((hash_c % self.n) / 64) as usize;
+// 		let rc = (hash_c % self.n) % 64;
+// 		let c = self.bits[ic] & 1 << rc != 0;
+//
+// 		// Combos
+// 		let iab = (((hash_a ^ hash_b) % self.n) / 64) as usize;
+// 		let rab = ((hash_a ^ hash_b) % self.n) % 64;
+// 		let ab = self.bits2[iab] & 1 << rab != 0;
+//
+// 		let ibc = (((hash_b ^ hash_c) % self.n) / 64) as usize;
+// 		let rbc = ((hash_b ^ hash_c) % self.n) % 64;
+// 		let bc = self.bits2[ibc] & 1 << rbc != 0;
+//
+// 		let ica = (((hash_a ^ hash_c) % self.n) / 64) as usize;
+// 		let rca = ((hash_a ^ hash_c) % self.n) % 64;
+// 		let ca = self.bits2[ica] & 1 << rca != 0;
+//
+// 		let iabc = (((hash_a ^ hash_b ^ hash_c) % self.n) / 64) as usize;
+// 		let rabc = ((hash_a ^ hash_b ^ hash_c) % self.n) % 64;
+// 		let abc = self.bits2[iabc] & 1 << rabc != 0;
+// 		a && b && c && ab && bc && ca && abc
+// 	}
+//
+// 	fn insert(&mut self, hash_a: u64, hash_b: u64, hash_c: u64) {
+// 		if self.contains(hash_a, hash_b, hash_c) {
+// 			return
+// 		}
+// 		self.count += 1;
+//
+// 		let ia = ((hash_a % self.n) / 64) as usize;
+// 		let ra = (hash_a % self.n) % 64;
+// 		self.bits[ia] |= 1 << ra;
+//
+// 		let ib = ((hash_b % self.n) / 64) as usize;
+// 		let rb = (hash_b % self.n) % 64;
+// 		self.bits[ib] |= 1 << rb;
+//
+// 		let ic = ((hash_c % self.n) / 64) as usize;
+// 		let rc = (hash_c % self.n) % 64;
+// 		self.bits[ic] |= 1 << rc;
+//
+// 		// Combos
+// 		let iab = (((hash_a ^ hash_b) % self.n) / 64) as usize;
+// 		let rab = ((hash_a ^ hash_b) % self.n) % 64;
+// 		self.bits2[iab] |= 1 << rab;
+//
+// 		let ibc = (((hash_c ^ hash_b) % self.n) / 64) as usize;
+// 		let rbc = ((hash_c ^ hash_b) % self.n) % 64;
+// 		self.bits2[ibc] |= 1 << rbc;
+//
+// 		let ica = (((hash_a ^ hash_c) % self.n) / 64) as usize;
+// 		let rca = ((hash_a ^ hash_c) % self.n) % 64;
+// 		self.bits2[ica] |= 1 << rca;
+//
+// 		let iabc = (((hash_a ^ hash_b ^ hash_c) % self.n) / 64) as usize;
+// 		let rabc = ((hash_a ^ hash_b ^ hash_c) % self.n) % 64;
+// 		self.bits2[iabc] |= 1 << rabc;
+// 	}
+//
+// 	fn len(&self) -> usize {
+// 		self.count
+// 	}
+// }
 
-impl Bloom64 {
-	fn with_cardinality(n: usize) -> Bloom64 {
-		Bloom64 {
-			bits: vec![0; n],
-			n: 64 * n as u64,
-			count: 0,
-		}
-	}
-
-	fn contains(&self, hash_a: u64, hash_b: u64, hash_c: u64) -> bool {
-		let ia = ((hash_a % self.n) / 64) as usize;
-		let ra = (hash_a % self.n) % 64;
-		let a = self.bits[ia] & 1 << ra != 0;
-
-		let ib = ((hash_b % self.n) / 64) as usize;
-		let rb = (hash_b % self.n) % 64;
-		let b = self.bits[ib] & 1 << rb != 0;
-
-		let ic = ((hash_c % self.n) / 64) as usize;
-		let rc = (hash_c % self.n) % 64;
-		let c = self.bits[ic] & 1 << rc != 0;
-
-		// Combos
-		let iab = (((hash_a ^ hash_b) % self.n) / 64) as usize;
-		let rab = ((hash_a ^ hash_b) % self.n) % 64;
-		let ab = self.bits[iab] & 1 << rab != 0;
-
-		let ibc = (((hash_b ^ hash_c) % self.n) / 64) as usize;
-		let rbc = ((hash_b ^ hash_c) % self.n) % 64;
-		let bc = self.bits[ibc] & 1 << rbc != 0;
-
-		let ica = (((hash_a ^ hash_c) % self.n) / 64) as usize;
-		let rca = ((hash_a ^ hash_c) % self.n) % 64;
-		let ca = self.bits[ica] & 1 << rca != 0;
-		a && b && c && ab && bc && ca
-	}
-
-	fn insert(&mut self, hash_a: u64, hash_b: u64, hash_c: u64) {
-		if self.contains(hash_a, hash_b, hash_c) {
-			return
-		}
-		self.count += 1;
-
-		let ia = ((hash_a % self.n) / 64) as usize;
-		let ra = (hash_a % self.n) % 64;
-		self.bits[ia] |= 1 << ra;
-
-		let ib = ((hash_b % self.n) / 64) as usize;
-		let rb = (hash_b % self.n) % 64;
-		self.bits[ib] |= 1 << rb;
-
-		let ic = ((hash_c % self.n) / 64) as usize;
-		let rc = (hash_c % self.n) % 64;
-		self.bits[ic] |= 1 << rc;
-
-		// Combos
-		let iab = (((hash_a ^ hash_b) % self.n) / 64) as usize;
-		let rab = ((hash_a ^ hash_b) % self.n) % 64;
-		self.bits[iab] |= 1 << rab;
-
-		let ibc = (((hash_c ^ hash_b) % self.n) / 64) as usize;
-		let rbc = ((hash_c ^ hash_b) % self.n) % 64;
-		self.bits[ibc] |= 1 << rbc;
-
-		let ica = (((hash_a ^ hash_c) % self.n) / 64) as usize;
-		let rca = ((hash_a ^ hash_c) % self.n) % 64;
-		self.bits[ica] |= 1 << rca;
-	}
-
-	fn len(&self) -> usize {
-		self.count
-	}
-}
-
-#[inline]
-fn push_clone(v: &Vec<usize>, a: usize) -> Vec<usize> {
-	let mut v2 = v.clone();
-	v2.push(a);
-	v2
-}
+// struct Bits32 {
+// 	bits: Vec<u64>,
+// 	count: usize,
+// }
+//
+// impl Bits32 {
+// 	fn new() -> Bits32 {
+// 		Bits32 {
+// 			bits: vec![0; 67_108_864],
+// 			count: 0,
+// 		}
+// 	}
+//
+// 	fn contains(&self, hash: u32) -> bool {
+// 		let ia = (hash / 64) as usize;
+// 		let ra = hash % 64;
+// 		let a = self.bits[ia] & 1 << ra != 0;
+// 		a
+// 	}
+//
+// 	fn insert(&mut self, hash: u32) {
+// 		if self.contains(hash) {
+// 			return
+// 		}
+// 		self.count += 1;
+//
+// 		let ia = (hash / 64) as usize;
+// 		let ra = hash % 64;
+// 		self.bits[ia] |= 1 << ra;
+// 	}
+//
+// 	fn len(&self) -> usize {
+// 		self.count
+// 	}
+// }
+//
+// #[inline]
+// fn push_clone(v: &Vec<usize>, a: usize) -> Vec<usize> {
+// 	let mut v2 = v.clone();
+// 	v2.push(a);
+// 	v2
+// }
 
 #[inline]
 fn sum(v: &[usize]) -> usize {
 	v.iter().fold(0, |s, a| s + a)
 }
 
-#[inline]
-fn max_3(a: usize, b: usize, c: usize) -> usize {
-	if a > b {
-		if a > c {a} else {c}
-	} else {
-		if b > c {b} else {c}
-	}
-}
+// #[inline]
+// fn max(v: &Vec<usize>) -> usize {
+// 	v.iter().fold(0, |m, a| if *a > m {*a} else {m})
+// }
+//
+// #[inline]
+// fn min(v: &Vec<usize>) -> usize {
+// 	v.iter().fold(usize::max_value(), |m, a| if *a < m {*a} else {m})
+// }
+//
+// #[inline]
+// fn max_diff(vecs: &Vec<usize>) -> usize {
+// 	max(vecs) - min(vecs)
+// }
+//
+// fn hash(p: usize, tup: &Vec<Vec<usize>>) -> u32 {
+// 	let mut v: Vec<Vec<usize>> = tup.clone();
+// 	v.sort();
+// 	let mut hash = 1315423911u32;
+// 	hash ^= (hash << 5) + (p as u32) + (hash >> 2);
+//
+// 	for coll in v {
+// 		hash ^= (hash << 5) + 263 + (hash >> 2);
+// 		for i in coll {
+// 			hash ^= (hash << 5) + (i as u32) + (hash >> 2);
+// 		}
+// 		hash ^= (hash << 5) + 997 + (hash >> 2);
+// 	}
+// 	hash
+// }
+//
+// fn hash_2(p: usize, tup: &(&Vec<usize>, &Vec<usize>, &Vec<usize>)) -> u32 {
+// 	let mut v = vec![tup.0, tup.1, tup.2];
+// 	v.sort();
+// 	let mut hash = 0u32;
+// 	hash = (p as u64) + (hash << 6) + (hash << 16) - hash;
+//
+// 	for coll in v {
+// 		hash = 263 + (hash << 6) + (hash << 16) - hash;
+// 		for i in coll {
+// 			hash = (*i as u64) + (hash << 6) + (hash << 16) - hash;
+// 		}
+// 		hash = 997 + (hash << 6) + (hash << 16) - hash;
+// 	}
+// 	hash
+// }
+//
+// fn hash_3(p: usize, tup: &(&Vec<usize>, &Vec<usize>, &Vec<usize>)) -> u32 {
+// 	let mut v = vec![tup.0, tup.1, tup.2];
+// 	v.sort();
+// 	let mut hash = 0u32;
+// 	hash = (p as u64) + hash * 131;
+//
+// 	for coll in v {
+// 		hash = 263 + hash * 131;
+// 		for i in coll {
+// 			hash = (*i as u64) + hash * 131;
+// 		}
+// 		hash = 997 + hash * 131;
+// 	}
+// 	hash
+// }
 
-#[inline]
-fn min_3(a: usize, b: usize, c: usize) -> usize {
-	if a < b {
-		if a < c {a} else {c}
-	} else {
-		if b < c {b} else {c}
-	}
-}
-
-#[inline]
-fn max_diff(a: usize, b: usize, c: usize) -> usize {
-	max_3(a, b, c) - min_3(a, b, c)
-}
-
-fn hash(p: usize, tup: &(&Vec<usize>, &Vec<usize>, &Vec<usize>)) -> u64 {
-	let mut v = vec![tup.0, tup.1, tup.2];
-	v.sort();
-	let mut hash = 1315423911u64;
-	hash ^= (hash << 5) + (p as u64) + (hash >> 2);
-
-	for coll in v {
-		hash ^= (hash << 5) + 263 + (hash >> 2);
-		for i in coll {
-			hash ^= (hash << 5) + (*i as u64) + (hash >> 2);
-		}
-		hash ^= (hash << 5) + 997 + (hash >> 2);
-	}
-	hash
-}
-
-fn hash_2(p: usize, tup: &(&Vec<usize>, &Vec<usize>, &Vec<usize>)) -> u64 {
-	let mut v = vec![tup.0, tup.1, tup.2];
-	v.sort();
-	let mut hash = 0u64;
-	hash = (p as u64) + (hash << 6) + (hash << 16) - hash;
-
-	for coll in v {
-		hash = 263 + (hash << 6) + (hash << 16) - hash;
-		for i in coll {
-			hash = (*i as u64) + (hash << 6) + (hash << 16) - hash;
-		}
-		hash = 997 + (hash << 6) + (hash << 16) - hash;
-	}
-	hash
-}
-
-fn hash_3(p: usize, tup: &(&Vec<usize>, &Vec<usize>, &Vec<usize>)) -> u64 {
-	let mut v = vec![tup.0, tup.1, tup.2];
-	v.sort();
-	let mut hash = 0u64;
-	hash = (p as u64) + hash * 131;
-
-	for coll in v {
-		hash = 263 + hash * 131;
-		for i in coll {
-			hash = (*i as u64) + hash * 131;
-		}
-		hash = 997 + hash * 131;
-	}
-	hash
-}
-
-fn equal_parts_3(p: &[usize], prog: (&Vec<usize>, &Vec<usize>, &Vec<usize>), memo: &mut Bloom64, memo2: &mut HashSet<u64>)
-						    -> Vec<(Vec<usize>, Vec<usize>, Vec<usize>)> {
-	if p.len() == 0 {
-		let sa = sum(&prog.0);
-		let sb = sum(&prog.1);
-		let sc = sum(&prog.2);
-		return if sa == sb && sa == sc { // Only return if this is a valid solution
-			vec![(prog.0.clone(), prog.1.clone(), prog.2.clone())]
-		} else {
-			vec![]
-		}
-	}
-
-	let d = max_diff(sum(&prog.0), sum(&prog.1), sum(&prog.2));
-	if d > sum(p) {
-		return vec![]  // if we'll never catch up our smallest to our biggest, abort
-	}
-
-	let key  = hash(p[0], &prog); // Compute our key, check our memo
-	let key2 = hash_2(p[0], &prog);
-	let key3 = hash_3(p[0], &prog);
-	if memo.contains(key, key2, key3) && memo2.contains(&key3) {
-		return vec![]
-	}
-
-	// Otherwise, we'll actually proceed with the computation
-	memo.insert(key, key2, key3);
-	memo2.insert(key3);
-	let mut a = equal_parts_3(&p[1..], (&push_clone(&prog.0, p[0]), prog.1, prog.2), memo, memo2);
-	let mut b = equal_parts_3(&p[1..], (prog.0, &push_clone(&prog.1, p[0]), prog.2), memo, memo2);
-	let mut c = equal_parts_3(&p[1..], (prog.0, prog.1, &push_clone(&prog.2, p[0])), memo, memo2);
-	if b.len() > 0 {
-		a.append(&mut b);
-	}
-	if c.len() > 0 {
-		a.append(&mut c);
-	}
-	a
-}
+// fn equal_parts_n(p: &[usize], prog: Vec<Vec<usize>>, memo: &mut Bits32, target: usize)
+// 						    -> LinkedList<Vec<Vec<usize>>> {
+// 	let c = if p.len() > 0 {p[0]} else {211};
+// 	let key  = hash(c, &prog); // Compute our key, check our memo
+// 	if memo.contains(key) {
+// 		return LinkedList::new()
+// 	}
+// 	// Otherwise, we'll actually proceed with the computation
+// 	memo.insert(key);
+//
+// 	let sums: Vec<usize> = prog.iter().map(|p| sum(p)).collect();
+// 	if p.len() == 0 {
+// 		let mut ll = LinkedList::new();
+// 		if sums.iter().fold(true, |b, i| b && *i == target) { // Only return if this is a valid solution
+// 			ll.push_back(vec![prog[0].clone(), prog[1].clone(), prog[2].clone()]);
+// 		}
+// 		return ll
+// 	}
+//
+// 	if max_diff(&sums) > sum(p) {
+// 		return LinkedList::new() // Once we build too far on one partition, abort
+// 	}
+//
+// 	let mut ll: LinkedList<Vec<Vec<usize>>> = LinkedList::new();
+// 	for i in 0..prog.len() {
+// 		if sum(&prog[i]) + p[0] > target {
+// 			continue;
+//  		}
+// 		let mut v: Vec<Vec<usize>> = Vec::new();
+// 		for j in 0..prog.len() {
+// 			if j != i {
+// 				v.push(prog[j].clone());
+// 			}
+// 		}
+// 		v.push(push_clone(&prog[i], p[0]));
+// 		let mut a = equal_parts_n(&p[1..], v, memo, target);
+// 		ll.append(& mut a);
+// 	}
+// 	ll
+// }
 
 pub fn part1(input: String) -> String {
-	let packages = parse_only(all_lines, input.as_bytes()).unwrap();
-	let mut set: Bloom64 = Bloom64::with_cardinality(50000017);
-	let mut set2: HashSet<u64> = HashSet::with_capacity(50000017);
-	let n = equal_parts_3(&packages[..], (&vec![], &vec![], &vec![]), &mut set, &mut set2).len();
-	println!("Cardinalities. Bloom {}, HT: {}, (-{})", set.len(), set2.len(), set2.len() - set.len());
-	format!("{:?}", n)
+	let mut packages = parse_only(all_lines, input.as_bytes()).unwrap();
+	let mut set: Bits32 = Bits32::new();
+	let target = sum(&packages);
+	println!("Target: {}", target);
+	packages.reverse();
+	let mut parts = equal_parts_n(&packages[2..], vec![vec![packages[0], packages[1]], vec![], vec![]], &mut set, target);
+	// parts.iter_mut().map(|&mut tup| )
+	format!("{:?}", parts.len())
 }
 
 
