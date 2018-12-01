63adb3a28b21c82b040909397b1afc068b596de6
Josiah Gaskin <jcg@fb.com>
2018-11-30 21:11:24 -0800

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,25 @@
+use std::collections::HashSet;
+
 pub fn part1(input: String) -> String {
-    format!("{}", input.lines().count())
+    format!("{}", 
+        input.lines()
+            .map(|l| l.parse::<i32>().unwrap())
+            .fold(0, |s, n| s + n)
+    )
 }
 
 pub fn part2(input: String) -> String {
-    format!("{}", input.lines().count())
+    let mut set: HashSet<i32> = HashSet::new();
+    let mut s = 0;
+    set.insert(0);
+    let values: Vec<i32> = input.lines().map(|l| l.parse::<i32>().unwrap()).collect();
+    for i in 0.. {
+        let n = values[i%values.len()];
+        s += n;
+        if set.contains(&s) {
+            return format!("{}",  s)
+        }
+        set.insert(s);
+    }
+    return "No Answer".to_owned()
 }
