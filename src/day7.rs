c77852cbb264615fa247dc98be4dd7501404198e
Josiah Gaskin <josiahgaskin@gmail.com>
2021-12-07 20:51:56 -0800

    day7

--- src/day7.rs
+++ src/day7.rs
@@ -1,8 +1,18 @@
 pub fn part1(input: String) -> String {
-	"part1".to_string()
+	let mut nums = input.split(",").map(|s| parse!(s, isize)).collect::<Vec<_>>();
+	let min = *nums.iter().min().unwrap();
+	let max = *nums.iter().max().unwrap();
+	
+	(min..=max).map(|d| nums.iter().map(|n| (n-d).abs()).sum::<isize>()).min().unwrap().to_string()
+
 }
 
 
 pub fn part2(input: String) -> String {
-	"part2".to_string()
+	let mut nums = input.split(",").map(|s| parse!(s, isize)).collect::<Vec<_>>();
+	let min = *nums.iter().min().unwrap();
+	let max = *nums.iter().max().unwrap();
+	
+	(min..=max).map(|d| nums.iter().map(|n| (n-d).abs()).map(|c| (c*c + c)/2).sum::<isize>()).min().unwrap().to_string()
+
 }
