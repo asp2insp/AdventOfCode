2bb471f7c852b4fb29956a18e9364524635bb2ce
= <=josiah@josiahgaskin.com>
2021-12-11 20:52:53 -0800

    2018/da23 finally done. Was missing <= instead of <

--- src/twenty_eighteen/day23.rs
+++ src/twenty_eighteen/day23.rs
@@ -1,7 +1,9 @@
-use regex::*;
-use rayon::prelude::*;
-use itertools::*;
+use std::cmp::max_by;
+
 use crate::utils::*;
+use itertools::*;
+use rayon::prelude::*;
+use regex::*;
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 struct Bot {
@@ -13,17 +15,20 @@ struct Bot {
 
 impl Bot {
     fn dist(&self, other: &Bot) -> isize {
-        (self.x-other.x).abs() +
-        (self.y-other.y).abs() +
-        (self.z-other.z).abs()
+        (self.x - other.x).abs() + (self.y - other.y).abs() + (self.z - other.z).abs()
     }
 
     fn in_range(&self, other: &Bot) -> bool {
-        self.dist(other) < self.r
+        self.dist(other) <= self.r
     }
 
-    fn can_see(&self, p: (isize, isize, isize)) -> bool {
-        self.in_range(&Bot { x: p.0, y: p.1, z: p.2, r: 0 })
+    fn can_see(&self, p: &(isize, isize, isize)) -> bool {
+        self.in_range(&Bot {
+            x: p.0,
+            y: p.1,
+            z: p.2,
+            r: 0,
+        })
     }
 
     fn min(&self) -> isize {
@@ -34,20 +39,17 @@ impl Bot {
         self.x.max(self.y).max(self.z)
     }
 
-    fn pts(&self) -> Vec<(isize, isize, isize)> {
-        let mut res = vec![];
-        for z in self.z-self.r ..= self.z+self.r {
-            for y in self.y-self.r ..= self.y+self.r {
-                for x in self.x-self.r ..= self.x+self.r {
-                    let p = (x, y, z);
-                    if self.can_see(p) {
-                        res.push((x, y, z));
-                    }
-                }
-            }
+    fn can_see_sphere(&self, (sphere_p, sphere_r): &((isize, isize, isize), isize)) -> bool {
+        Bot {
+            r: self.r + sphere_r,
+            ..*self
         }
-        res
+        .can_see(sphere_p)
     }
+}
+
+fn origin_dist(cr: &((isize, isize, isize), isize)) -> isize {
+    cr.0 .0.abs() + cr.0 .1.abs() + cr.0 .2.abs()
 }
 
 fn parse_bots(s: &str) -> Vec<Bot> {
@@ -62,64 +64,91 @@ fn parse_bots(s: &str) -> Vec<Bot> {
         .collect()
 }
 
-fn rank_pos(p: (isize, isize, isize), bots: &[Bot]) -> usize {
+fn rank_pos(p: &(isize, isize, isize), bots: &[Bot]) -> usize {
     bots.iter().filter(|b| b.can_see(p)).count()
+}
+
+fn rank_sphere(s: &((isize, isize, isize), isize), bots: &[Bot]) -> usize {
+    bots.iter().filter(|b| b.can_see_sphere(s)).count()
 }
 
 pub fn part1(input: String) -> String {
     let bots = parse_bots(&input);
     let bigbot = bots.iter().max_by_key(|b| b.r).unwrap();
-    bots.iter().filter(|b| bigbot.in_range(b)).count().to_string()
+    bots.iter()
+        .filter(|b| bigbot.in_range(b))
+        .count()
+        .to_string()
 }
 
 fn anneal(p: (isize, isize, isize), param: isize) -> Vec<(isize, isize, isize)> {
     vec![
-        (p.0 + param, p.1, p.2), (p.0 - param, p.1, p.2),
-        (p.0, p.1 + param, p.2), (p.0 - param, p.1 - param, p.2),
-        (p.0, p.1, p.2 + param), (p.0, p.1, p.2 - param),
-        p
+        (p.0 + param, p.1, p.2),
+        (p.0 - param, p.1, p.2),
+        (p.0, p.1 + param, p.2),
+        (p.0 - param, p.1 - param, p.2),
+        (p.0, p.1, p.2 + param),
+        (p.0, p.1, p.2 - param),
+        p,
     ]
+}
+
+// Split a sphere into 8 sub-spheres which have the 8
+// cube-splits of space inscribed in them
+fn partition_space((c, r): &((isize, isize, isize), isize)) -> Vec<((isize, isize, isize), isize)> {
+    let shift = r / 2;
+    let rnew = (shift * 99) / 70; // approx(sqrt(2)/2)
+    let mut res = Vec::with_capacity(27);
+    for zchange in [-1, 0, 1] {
+        for ychange in [-1, 0, 1] {
+            for xchange in [-1, 0, 1] {
+                res.push((
+                    (
+                        c.0 + xchange * shift,
+                        c.1 + ychange * shift,
+                        c.2 + zchange * shift,
+                    ),
+                    rnew,
+                ));
+            }
+        }
+    }
+    res
 }
 
 pub fn part2(input: String) -> String {
     let bots = parse_bots(&input);
     let min = bots.iter().map(Bot::min).min().unwrap();
     let max = bots.iter().map(Bot::max).max().unwrap();
-    let mut vec = Vec::new();
-    let sample_dist = (max - min) / 100;
-    for z in (min..max).step_by(sample_dist as usize) {
-        for y in (min..max).step_by(sample_dist as usize) {
-            for x in (min..max).step_by(sample_dist as usize) {
-                vec.push(((x, y, z), 0));
+    let mut cr = ((0, 0, 0), (max - min));
+    while cr.1 > 100 {
+        // println!("best {}, {}", rank_sphere(&cr, &bots), cr.1);
+        cr = partition_space(&cr)
+            .into_iter()
+            .max_by(|s1, s2| {
+                rank_sphere(s1, &bots).cmp(&rank_sphere(s2, &bots))
+                    .then(origin_dist(s2).cmp(&origin_dist(s1)))
+            })
+            .unwrap();
+    }
+    // Below r = 100, search every point
+    let mut best = (cr.0, 0);
+    for z in cr.0.2 - cr.1 .. cr.0.2 + cr.1 {
+        for y in cr.0.1 - cr.1 .. cr.0.1 + cr.1 {
+            for x in cr.0.0 - cr.1 .. cr.0.0 + cr.1 {
+                best = max_by(best, ((x, y, z), 0), |s1, s2| {
+                    rank_sphere(s1, &bots).cmp(&rank_sphere(s2, &bots))
+                        .then(origin_dist(s2).cmp(&origin_dist(s1)))
+                });
             }
         }
+        // println!("best {}, {}, {}, {}", z, rank_sphere(&best, &bots), best.1, origin_dist(&best));
     }
-    for b in &bots {
-        vec.push(((b.x, b.y, b.z), 1));
-    }
-
-    let gen_size = vec.len();
-    let mut cap = (max - min) / 100;
-    while cap > 0 {
-        // Run 10 generations at each step size
-        for _ in 0..10 {
-            // In each generation, anneal every point, sort by cost,
-            // then cull back to gensize
-            vec = vec.into_par_iter()
-                .flat_map(|p| anneal(p.0, cap).into_par_iter())
-                .map(|p| (p, rank_pos(p, &bots)))
-                .collect();
-            vec = vec.into_iter().sorted_by_key(|p| usize::MAX - p.1).dedup().take(gen_size).collect();
-            println!("gen");
-        }
-        // In step sizes of 10
-        if cap < 10 {
-            cap -= 1;
-        } else {
-            cap /= 10;
-        }
-        println!("..{} ({})", cap, vec[0].1);
-    }
-    let results = vec[0..100].iter().sorted_by_key(|p| p.0.0.abs() + p.0.1.abs() + p.0.2.abs()).collect::<Vec<_>>();
-    format!("{:?}", &results[0..5])
+    format!(
+        "{:?}, {}|{} = {}",
+        best,
+        rank_sphere(&best, &bots),
+        rank_pos(&best.0, &bots),
+        origin_dist(&best)
+    )
 }
