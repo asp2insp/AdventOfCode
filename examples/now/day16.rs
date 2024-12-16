bd00aec7396e31e57b6d8a5095e199ad6413fb5b
= <=josiah@josiahgaskin.com>
2024-12-15 23:16:08 -0800

    2024:16 done

--- examples/now/day16.rs
+++ examples/now/day16.rs
@@ -1,8 +1,10 @@
-use std::{collections::{HashMap, VecDeque}, rc::Rc};
+use std::{
+    collections::{HashMap, VecDeque},
+    rc::Rc,
+};
 
-use aoc::utils::*;
+use aoc::{makeset, utils::*};
 use fnv::FnvHashSet;
-use rayon::prelude::*;
 
 pub fn part1(input: String) -> String {
     let g = Grid::new(&input, ()).with_wall('#');
@@ -34,81 +36,75 @@ pub fn part1(input: String) -> String {
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct PL {
-	pos: Point,
-	next: Option<Rc<PL>>,
+    pos: Point,
+    next: Option<Rc<PL>>,
 }
 
 impl PL {
-	fn new(pos: Point) -> Self {
-		Self {
-			pos,
-			next: None,
-		}
-	}
+    fn new(pos: Point) -> Self {
+        Self { pos, next: None }
+    }
 
-	fn clone_with(self: &Rc<Self>, pos: Point) -> Rc<Self> {
-		Rc::new(Self {
-			pos,
-			next: Some(self.clone()),
-		})
-	}
-
-	fn contains(&self, pos: &Point) -> bool {
-		if &self.pos == pos {
-			true
-		} else if let Some(next) = &self.next {
-			next.contains(pos)
-		} else {
-			false
-		}
-	}
+    fn clone_with(self: &Rc<Self>, pos: Point) -> Rc<Self> {
+        Rc::new(Self {
+            pos,
+            next: Some(self.clone()),
+        })
+    }
 }
 
 pub fn part2(input: String) -> String {
-	let target_cost = part1(input.clone()).parse::<usize>().unwrap();
+    let target_cost = part1(input.clone()).parse::<usize>().unwrap();
     let g = Grid::new(&input, ()).with_wall('#');
     let start = g.find('S').unwrap();
     let end = g.find('E').unwrap();
     let mut q = VecDeque::new();
     q.push_back((start, Direction::E, 0, Rc::new(PL::new(Point::new(-1, -1)))));
-    let mut trails = vec![];
+    let mut best_path_to_point: HashMap<(Point, Direction), (usize, Vec<Rc<PL>>)> = HashMap::new();
     while let Some((pos, dir, steps, trail)) = q.pop_front() {
-        if pos == end {
-            if steps == target_cost {
-				trails.push(trail);
-			}
+        if steps > target_cost {
             continue;
         }
-		if steps > target_cost {
-			continue;
-		}
-        if trail.contains(&pos) {
+        if !best_path_to_point.contains_key(&(pos, dir)) {
+            best_path_to_point.insert((pos, dir), (usize::MAX, Vec::<Rc<PL>>::new()));
+        }
+        let (point_cost, trails) = best_path_to_point.get_mut(&(pos, dir)).unwrap();
+        if steps < *point_cost {
+            *point_cost = steps;
+            *trails = vec![trail.clone()];
+        } else if steps == *point_cost {
+            trails.push(trail.clone());
+        } else {
+            continue;
+        }
+        if pos == end {
             continue;
         }
         if let Some(next) = g.drive(pos, dir) {
             q.push_front((next, dir, steps + 1, trail.clone_with(pos)));
         }
-		if g.drive(pos, dir.turn('L')).is_some() {
-			q.push_front((pos, dir.turn('L'), steps + 1000, trail.clone()));
-		}
-		if g.drive(pos, dir.turn('R')).is_some() {
-			q.push_front((pos, dir.turn('R'), steps + 1000, trail.clone()));
-		}
+        if g.drive(pos, dir.turn('L')).is_some() {
+            q.push_front((pos, dir.turn('L'), steps + 1000, trail.clone()));
+        }
+        if g.drive(pos, dir.turn('R')).is_some() {
+            q.push_front((pos, dir.turn('R'), steps + 1000, trail.clone()));
+        }
     }
-    trails.push(Rc::new(PL::new(end)));
-    trails
-        .into_iter()
+    (DIRECTIONS.clone()
+		.into_iter()
+		.flat_map(|d| best_path_to_point.get(&(end, d)))
+        .flat_map(|(_, trails)| trails.clone())
         .flat_map(|mut pl| {
-			let mut v = vec![pl.pos];
-			while let Some(next) = &pl.next {
-				pl = next.clone();
-				v.push(pl.pos);
-			}
-			v
-		})
-		.filter(|&p| p != Point::new(-1, -1))
+            let mut v = vec![pl.pos];
+            while let Some(next) = &pl.next {
+                pl = next.clone();
+                v.push(pl.pos);
+            }
+            v
+        })
+        .filter(|&p| p != Point::new(-1, -1))
         .collect::<FnvHashSet<Point>>()
-        .len()
+        .len() + 1)
         .to_string()
 }
 
